5eaa1018649ed8f7e13a556ff8dc6076d82e1003
Thales Pereira <thales.claro@gmail.com>
2018-10-31 16:47:53 +0000

    Cleanup and simplification

--- run.sh
+++ run.sh
@@ -7,16 +7,17 @@ source config.env
 
 DOCKER_IMAGE="jenkins-agent-$AGENT_NAME"
 
+docker rm -f $DOCKER_IMAGE
+
 echo "Build Image"
 docker build -t $DOCKER_IMAGE .
 
 echo "Launch agent"
-docker run -d  \
+docker run --restart on-failure  \
 	-e JNLP_AGENT_URL=$JNLP_AGENT_URL\
 	-e JENKINS_SECRET=$JENKINS_SECRET \
 	-e TARGET=$TARGET \
 	-e JNLP_AGENT_DOWNLOAD_URL=$JNLP_AGENT_DOWNLOAD_URL \
-	-e JENKINS_WORKDIR=$JENKINS_WORKDIR \
 	-v /var/run/docker.sock:/var/run/docker.sock \
 	-v $JENKINS_WORKDIR:$JENKINS_WORKDIR \
 	--name="$DOCKER_IMAGE" \
--- worker.sh
+++ worker.sh
@@ -1,34 +1,10 @@
 #!/bin/bash
 set -e
 
-agent_runner() {
-    while :
-    do
-        if [ ! -f "/proc/$(cat /tmp/agent.pid)/status" ]
-        then
-            curl $JNLP_AGENT_DOWNLOAD_URL -o agent.jar
-            java \
-            -Dorg.jenkinsci.plugins.durabletask.BourneShellScript.HEARTBEAT_CHECK_INTERVAL=300 \
-            -Dhttps.protocols=TLSv1.2 \
-            -jar agent.jar \
-            -jnlpUrl $JNLP_AGENT_URL \
-            -secret $JENKINS_SECRET \
-            -workDir "$JENKINS_WORKDIR" &
-            echo $! > /tmp/agent.pid
-        else
-            :
-        fi
-        sleep 10
-    done
-}
-
-while :
-do
-    if [ cat < /dev/tcp/"$TARGET" ]; then
-      echo "Starting Agent"
-      agent_runner
-    else
-      echo "Jenkins master is offline, waiting...."
-    fi
-    sleep 10
-done
+curl $JNLP_AGENT_DOWNLOAD_URL -o agent.jar
+java \
+    -Dorg.jenkinsci.plugins.durabletask.BourneShellScript.HEARTBEAT_CHECK_INTERVAL=300 \
+    -Dhttps.protocols=TLSv1.2 \
+    -jar agent.jar \
+    -jnlpUrl $JNLP_AGENT_URL \
+    -secret $JENKINS_SECRET
