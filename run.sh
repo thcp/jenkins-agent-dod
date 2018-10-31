2c6005add08c800355dcd8a366ec7c6c55388e8b
Thales Pereira <31625914+thcp@users.noreply.github.com>
2018-10-31 11:36:06 +0000

    Build image on every execution.

--- run.sh
+++ run.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-case $1 in
-	build)
-		docker build -t jenkins-slave .
-		;;
-	run)
-	source config.env
-	docker run -d  \
-		-e JNLP_AGENT_URL=$JNLP_AGENT_URL\
-		-e JENKINS_SECRET=$JENKINS_SECRET \
-		-e TARGET=$TARGET \
-		-e JNLP_AGENT_DOWNLOAD_URL=$JNLP_AGENT_DOWNLOAD_URL \
-		-e JENKINS_WORKDIR=$JENKINS_WORKDIR \
-		-v /var/run/docker.sock:/var/run/docker.sock \
-		-v $JENKINS_WORKDIR:$JENKINS_WORKDIR \
-		--name=jenkins-slave \
-		jenkins-slave
-		;;
-esac
+set -e
+
+source config.env
+
+echo "Build Image"
+docker build -t jenkins-slave .
+
+echo "Launch agent"
+docker run -d  \
+	-e JNLP_AGENT_URL=$JNLP_AGENT_URL\
+	-e JENKINS_SECRET=$JENKINS_SECRET \
+	-e TARGET=$TARGET \
+	-e JNLP_AGENT_DOWNLOAD_URL=$JNLP_AGENT_DOWNLOAD_URL \
+	-e JENKINS_WORKDIR=$JENKINS_WORKDIR \
+	-v /var/run/docker.sock:/var/run/docker.sock \
+	-v $JENKINS_WORKDIR:$JENKINS_WORKDIR \
+	--name=jenkins-slave \
+	jenkins-slave
